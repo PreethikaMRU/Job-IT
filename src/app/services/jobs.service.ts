430047e542ac4a903a823e94e2cdc830232aeeed
Preethika <preethikamru@gmail.com>
2023-08-15 22:00:22 +0530

    Updated service

--- src/app/services/jobs.service.ts
+++ src/app/services/jobs.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders} from '@angular/common/http';
 // import {key} from "../../key"
-import { environment} from 'src/environments/environment';
+// import { environment} from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
